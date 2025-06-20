404015bf730e91fdd7fe55a4736a2937d44b8e4e
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-06-20 01:11:54 +0100

    ch

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,3 +1,7 @@
 let some: string | number = 'Hello';
+some = 123;
+
 let status: 'enable' | 'disable' = 'enable';
-console.log(some, status);
+
+console.log('some:', some);
+console.log('status:', status);
